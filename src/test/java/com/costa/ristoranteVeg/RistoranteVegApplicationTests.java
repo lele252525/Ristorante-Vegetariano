2b9c53c5eae9411b39d627fb1e8664432add7173
lele252525 <costadaniele2016@gmail.com>
2025-09-17 08:56:47 +0200

    Primo commit

---
+++ src/test/java/com/costa/ristoranteVeg/RistoranteVegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.costa.ristoranteVeg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RistoranteVegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
